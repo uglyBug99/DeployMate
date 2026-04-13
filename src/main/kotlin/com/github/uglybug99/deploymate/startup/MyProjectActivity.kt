2c10a6f46e79fd3879da014e197dc53869bc81d8
GitHub Action <action@github.com>
2026-04-13 05:52:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/uglybug99/deploymate/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/uglybug99/deploymate/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.uglybug99.deploymate.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
